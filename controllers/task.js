d6a76baf7b254dfdd91b15e10b45d0a4cb71a6bb
rakhawardhana <rakhagustiwardhana@gmail.com>
2020-05-28 23:02:33 +0700

    file storage

--- controllers/task.js
+++ controllers/task.js
@@ -5,73 +5,138 @@ const fs = require('fs')
 const rootdir = path.join(__dirname , '/../..' )
 const productdir = path.join (rootdir , 'konvergen.ai_test_backend/files/datasets')
 
+const {Storage} = require('@google-cloud/storage');
+const admin = require("firebase-admin");
 
-const folder = multer.diskStorage(
-    {
-        destination: function(req,file,cb){
-            cb(null,productdir)
-        },
-        filename: function(req,file,cb){
-            cb(null, Date.now() + file.fieldname + path.extname(file.originalname))
+const serviceAccount = require("../service_account.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "rakha96.appspot.com"
+});
+
+const bucket = admin.storage().bucket();
+
+// const folder = multer.diskStorage(
+//     {
+//         destination: function(req,file,cb){
+//             cb(null,productdir)
+//         },
+//         filename: function(req,file,cb){
+//             cb(null, Date.now() + file.fieldname + path.extname(file.originalname))
+//         }
+//     }
+// )
+
+// const upstore = multer (
+//     {
+//         storage : folder,
+//         limits : {
+//             fileSize : 20000000000000 
+//         },
+//         fileFilter(req,file,cb){
+//             let photo = file.originalname.match(/\.(zip)$/)
+
+//             if(!photo){
+//                 cb(new Error('Masukan file dengan extensi zip'))
+//             }
+
+//             cb(undefined, true)
+//         }
+//     }
+// )
+
+// const storage = new Storage({
+//     projectId: "rakha96",
+//     keyFilename: "../service_account.json"
+// })
+
+// const bucket = storage.bucket("gs://rakha96.appspot.com");
+
+const uploadImageToStorage = (file) => {
+    return new Promise((resolve, reject) => {
+      if (!file) {
+        reject('No image file');
+      }
+    //   Date.now() + file.fieldname + path.extname(file.originalname)
+      let newFileName = `${Date.now()}${file.fieldname}${path.extname(file.originalname)}`;
+
+      let fileUpload = bucket.file(newFileName);
+      const blobStream = fileUpload.createWriteStream({
+        metadata: {
+          contentType: file.mimetype
         }
-    }
-)
+      });
+  
+      blobStream.on('error', (error) => {
+          console.error(error)
+        reject('Something is wrong! Unable to upload at the moment.');
+      });
+  
+      blobStream.on('finish', () => {
+        // The public URL can be used to directly access the file via HTTP.
 
-const upstore = multer (
-    {
-        storage : folder,
-        limits : {
-            fileSize : 20000000000000 
-        },
-        fileFilter(req,file,cb){
-            let photo = file.originalname.match(/\.(zip)$/)
-
-            if(!photo){
-                cb(new Error('Masukan file dengan extensi zip'))
-            }
-
-            cb(undefined, true)
-        }
-    }
-)
-
+        fileUpload.getSignedUrl({
+            action: 'read',
+            expires: '03-09-2341'
+        }).then(urls => resolve(urls[0]))
+      });
+  
+      blobStream.end(file.buffer);
+    });
+}
 
 // admin create new task
 exports.create = (req, res, next)  => {
     const data = req.body
     console.log(req.body)
     var sql = "INSERT INTO tasks(name, description, dataset, booked, admin_id) VALUES (?, ?, ?, ?, ?)"
+    const file = req.file
 
-    db.run(sql, [req.body.name, req.body.description, req.file.filename, 0, parseInt(data.admin_id) ], (err, result) => {
-        if (err){
-            res.status(400).json({"error": err.message})
-        }
-        res.json({
-            "message": "success"
-        })
-    })
+    if (file) {
+        uploadImageToStorage(file)
+            .then(fileUrl => {
+                db.run(sql, [req.body.name, req.body.description, fileUrl, 0, parseInt(data.admin_id) ], (err, result) => {
+                    if (err){
+                        res.status(400).json({"error": err.message})
+                    }
+                    res.json({
+                        "message": "success"
+                    })
+                })
+            })
+    }
+    
 }
 
 exports.editTask = (req, res, next) => {
     console.log(req.file)
-    const tasks = {
-        name: req.body.name, 
-        description: req.body.description, 
-        dataset: req.file.filename, 
-        // dataset: req.file.name, 
-        booked: req.body.booked, 
-        admin_id: req.body.admin_id
-
-    }
+    const file = req.file
     const sql = `update tasks SET name = ?, description = ?, dataset =  ?, booked = ?, admin_id = ? where id = ?`
     const paramsId = req.params.id    
-    db.run(sql, tasks.name, tasks.description, tasks.dataset, tasks.booked, tasks.admin_id, paramsId, (err, result) => {
-        if(err){
-            console.log(err)
-            res.send(err)
-        }
-        res.send(result)
-    })
+    if (file) {
+        uploadImageToStorage(file)
+            .then(fileUrl => {
+
+                const tasks = {
+                    name: req.body.name, 
+                    description: req.body.description, 
+                    dataset: fileUrl, 
+                    // dataset: req.file.name, 
+                    booked: req.body.booked, 
+                    admin_id: req.body.admin_id
+
+                }
+                db.run(sql, tasks.name, tasks.description, tasks.dataset, tasks.booked, tasks.admin_id, paramsId, (err, result) => {
+                    if(err){
+                        console.log(err)
+                        res.send(err)
+                    }
+                    res.send(result)
+                })
+            })
+    }
+    
 
 }
 
--- routes/task.js
+++ routes/task.js
@@ -8,47 +8,69 @@ const authenticationMiddleware = require('../middleware/authentication')
 const router = express.Router();
 
 const path = require('path')
-const multer = require('multer')
+const Multer = require('multer')
 const fs = require('fs')
+
+
 const rootdir = path.join(__dirname , '/../..' )
 const productdir = path.join (rootdir , 'konvergen.ai_test_backend/files/datasets')
 
 
-const folder = multer.diskStorage(
-    {
-        destination: function(req,file,cb){
-            cb(null,productdir)
-        },
-        filename: function(req,file,cb){
-            cb(null, Date.now() + file.fieldname + path.extname(file.originalname))
+
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 20000000000000
+    },
+    fileFilter(req,file,cb){
+        let photo = file.originalname.match(/\.(zip)$/)
+
+        if(!photo){
+            cb(new Error('Masukan file dengan extensin zip'))
         }
+
+        cb(undefined, true)
     }
-)
+})
 
-const upstore = multer (
-    {
-        storage : folder,
-        limits : {
-            fileSize : 20000000000000 //bytes
-        },
-        fileFilter(req,file,cb){
-            let photo = file.originalname.match(/\.(zip)$/)
+// const folder = multer.diskStorage(
+//     {
+//         destination: function(req,file,cb){
+//             cb(null,productdir)
+//         },
+//         filename: function(req,file,cb){
+//             cb(null, Date.now() + file.fieldname + path.extname(file.originalname))
+//         }
+//     }
+// )
 
-            if(!photo){
-                cb(new Error('Masukan file dengan extensin zip'))
-            }
+// const upstore = multer (
+//     {
+//         storage : folder,
+//         limits : {
+//             fileSize : 20000000000000 //bytes
+//         },
+//         fileFilter(req,file,cb){
+//             let photo = file.originalname.match(/\.(zip)$/)
 
-            cb(undefined, true)
-        }
-    }
-)
+//             if(!photo){
+//                 cb(new Error('Masukan file dengan extensin zip'))
+//             }
+
+//             cb(undefined, true)
+//         }
+//     }
+// )
 
 
 
 
 // manage dataset
-router.post('/createTask', authenticationMiddleware, checkAdmin, upstore.single('datasets'), taskController.create); // create task and upload zip
-router.patch('/editTask/:id', authenticationMiddleware, checkAdmin, upstore.single('datasets'), taskController.editTask ) //  edit task and dataset
+// router.post('/createTask', authenticationMiddleware, checkAdmin, upstore.single('datasets'), taskController.create); // create task and upload zip
+// router.patch('/editTask/:id', authenticationMiddleware, checkAdmin, upstore.single('datasets'), taskController.editTask ) //  edit task and dataset
+
+router.post('/createTask', authenticationMiddleware, checkAdmin, multer.single('datasets'), taskController.create); // create task and upload zip
+router.patch('/editTask/:id', authenticationMiddleware, checkAdmin, multer.single('datasets'), taskController.editTask ) //  edit task and dataset
 
 
 // manage task
@@ -57,7 +79,7 @@ router.get('/:id', authenticationMiddleware, taskController.get) // get task by 
 router.get('/task/:admin_id', authenticationMiddleware, checkAdmin, taskController.getByAdmin) // get task by admin
 router.patch('/bookingTask/:id', authenticationMiddleware, checkAdmin, taskController.bookingTask)// booking task
 router.patch('/revokingTask/:id', authenticationMiddleware, checkAdmin, taskController.revokingTask)// revoking task
-router.get('/downloadDataset/:dataset', authenticationMiddleware, taskController.downloadDataset) // download dataset
+router.get('/downloadDataset/:dataset', taskController.downloadDataset) // download dataset
 router.patch('/bookingTaskAnnotator/:id', authenticationMiddleware, taskController.bookingTaskByAnnotator)// booking task
 router.get('/alltask/:annotator_id', authenticationMiddleware, taskController.getAllByAnnotator) // tasks list by annotator
 router.patch('/disableTask/:id', authenticationMiddleware, checkAdmin, taskController.disableTask) // disable task
